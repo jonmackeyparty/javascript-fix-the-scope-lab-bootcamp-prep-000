e769f508825b7be954e3b158e91161aaa1f02f82
Jon Mackey <jon@jonmackey.party>
2018-07-23 19:12:40 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -10,9 +10,7 @@ describe('Fix the Scope', function() {
 
 function yourAnimal(){
   var yourAnimal='cat';
-  return callYourAnimal(){
-    return ${yourAnimal}
-  }
+  return yourAnimal
 }
   describe('yourAnimal()', function() {
     it('returns your animal', function(){
