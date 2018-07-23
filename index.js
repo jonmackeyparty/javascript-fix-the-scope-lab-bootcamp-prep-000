4a0ca587911d9e71863be1af0c9b3826a22ccd71
Jon Mackey <jon@jonmackey.party>
2018-07-23 20:01:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,9 +19,7 @@ function add2(n) {
   // Feel free to move things around!
   
 var funkyFunction = function() {
-  return function() {
     return "FUNKY!"
-  }
 }
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
