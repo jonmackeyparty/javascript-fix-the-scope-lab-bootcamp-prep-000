d0af2f2bb65b308b9140070043cf256b9f34c05e
Jon Mackey <jon@jonmackey.party>
2018-07-23 19:47:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function add2(n) {
 }
   // Feel free to move things around!
   
-var funkyFunction = function() {
+var funkyFunction = function outerFunk() {
   return function() {
     return "FUNKY!"
   }
@@ -28,4 +28,4 @@ var funkyFunction = function() {
 // We want 'funkyFunction' on the line below to return a function that returns "FUNKY!" -- how can we accomplish that?
 // To pass this final test, you only need to modify the code below this line.
 
-var theFunk = funkyFunction();
+var theFunk = funkyFunction
