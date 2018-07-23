648571b25deef1c030542eadd4310c1cf97910a2
Jon Mackey <jon@jonmackey.party>
2018-07-23 20:11:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,6 +28,6 @@ var funkyFunction = function() {
 // NOTE: you only need to modify the code below this line.
 
 
-var funkTown=funcyFunction();
+var funkTown=funkyFunction();
 var theFunk = funkTown()
   
