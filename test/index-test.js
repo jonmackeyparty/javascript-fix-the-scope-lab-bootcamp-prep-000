8ddcda98eafd3dd8819c218f583cf8f5fe55933d
Jon Mackey <jon@jonmackey.party>
2018-07-23 19:03:42 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -18,9 +18,7 @@ describe('Fix the Scope', function() {
     })
   })
 
-function add2(n){
-  return n + 2
-}
+
   describe('add2(n)', function() {
     it('adds two to n', function() {
       const n = Math.floor(Math.random() * 1000)
